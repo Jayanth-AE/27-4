01f11dcac72c54195aed6db3f77be924cb26b655
Jayanth <jay496j@gmail.com>
2020-05-03 19:13:51 +0530

    ;lk

--- src/test/java/StepDefn/FBLoginSteps.java
+++ src/test/java/StepDefn/FBLoginSteps.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
